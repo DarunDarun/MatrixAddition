b93173d9bf9dea18190d3e36faf56fd6773aa2b1
Darun_D <88335680+DarunDarun@users.noreply.github.com>
2025-04-03 20:52:13 +0530

    Create MatrixAddition.java
    This code helps in Matrix Addition...

---
+++ MatrixAddition.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        int a[][]={{1,2,3,},{4,5,6},{1,2,3}};
+        int b[][]={{4,5,6},{1,2,3},{1,2,3}};
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(a[i][j]+b[i][j]+" ");
+
+            }
+            
+                
+            }
+        }
+    }
